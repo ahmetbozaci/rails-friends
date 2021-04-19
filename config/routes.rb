32250252f661494043420c4412d5f67925511065
Ahmet Bozaci <ahmt9417@gmail.com>
2021-04-19 16:10:31 +0300

    Create about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index' # http://127.0.0.1:3000
+  get 'home/index' # http://127.0.0.1:3000/home/index
+  # get 'index', to:'home#index' http://127.0.0.1:3000/index
+  get 'home/about'
 end
