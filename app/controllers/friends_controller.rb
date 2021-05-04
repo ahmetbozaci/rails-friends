e2de29175944d5862139a13768615bd012ffe1fd
Ahmet Bozaci <ahmt9417@gmail.com>
2021-05-04 16:31:20 +0300

    Add correct_user method

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: %i[ index show ]
+  before_action :correct_user, only: %i[ edit update destroy ]
 
   # GET /friends or /friends.json
   def index
@@ -12,7 +14,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    #@friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,8 +24,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
-
+    #@friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
     respond_to do |format|
       if @friend.save
         format.html { redirect_to @friend, notice: "Friend was successfully created." }
@@ -56,6 +59,11 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not Authorized" if @friend.nil?
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
@@ -64,6 +72,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
     end
 end
