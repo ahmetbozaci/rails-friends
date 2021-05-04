be74105d8e56451249623c85063029a72f2a47cf
Ahmet Bozaci <ahmt9417@gmail.com>
2021-05-04 18:27:50 +0300

    Add user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  root 'home#index' # http://127.0.0.1:3000
+  root 'friends#index'
+  # root 'home#index' # http://127.0.0.1:3000
   get 'home/index' # http://127.0.0.1:3000/home/index
   # get 'index', to:'home#index' http://127.0.0.1:3000/index
   get 'home/about'
+
 end
